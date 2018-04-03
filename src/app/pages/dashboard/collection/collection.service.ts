16d00536b28b52218d785ab91f3a63f8f2761878
zhangshuai <1091208884@qq.com>
2018-04-03 11:36:23 +0800

    4月3号张帅修改

--- src/app/pages/dashboard/collection/collection.service.ts
+++ src/app/pages/dashboard/collection/collection.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 @Injectable()
 export class CollectionService {
 
   constructor(private http:HttpClient) { }
   getCollection(){
-    return this.http.post("/tw-nms-cmts-server/homeCmtsCollectStatistics",{})
+    let path=environment.getPath()+'/tw-cmts-server/otherObject';
+    var json_Obj={
+      name:"1112",pwd:"xxx"
+    };
+    return this.http.post(path,toFormData(json_Obj),{
+      headers:new HttpHeaders({'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8'}),
+    });
   }
 }
